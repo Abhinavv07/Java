e1a314dc558033354fc9f6d8f6aca4497844f7f4
Archie-6 <abhinavkumarmishra2009@gmail.com>
2024-05-23 21:14:23 +0530

    update

--- java_development/src/Arrays/SearchingAndSorting.java
+++ java_development/src/Arrays/SearchingAndSorting.java
@@ -6,6 +6,7 @@ public class SearchingAndSorting {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+
         System.out.println("Enter value of n");
         int n = sc.nextInt();
 
