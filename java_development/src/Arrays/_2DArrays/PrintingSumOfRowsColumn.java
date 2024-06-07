43ef5288c03d7a3565edd5d153d82b8c05c29a89
Archie-6 <abhinavkumarmishra2009@gmail.com>
2024-06-07 11:52:22 +0530

    update

---
+++ java_development/src/Arrays/_2DArrays/PrintingSumOfRowsColumn.java
@@ -0,0 +1,46 @@
+package Arrays._2DArrays;
+
+import java.util.Scanner;
+
+public class PrintingSumOfRowsColumn {
+    public static void main(String []args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no of Rows");
+        int r = sc.nextInt();
+        System.out.println("Enter no of columns");
+        int c = sc.nextInt();
+
+        int [][]arr = new int[r][c];
+        int i,j;
+        for( i = 0; i < r; i++)
+        {
+            for( j = 0; j < c; j++)
+            {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+ //sum of rows
+        for( i = 0; i < r; i++)
+        {
+            int rsum = 0;
+            for(j = 0; j < c; j++)
+            {
+                rsum += arr[i][j];
+            }
+            System.out.println("Sum of " + (i + 1) + " rows is ;- " + rsum );
+        }
+
+        //for columns
+
+        for(i = 0; i < r; i++)
+        {
+            int csum = 0;
+            for(j = 0; j < c; j++)
+            {
+                csum += arr[j][i];
+            }
+            System.out.println("Sum of " + (i + 1) + " column is ;- " + csum );
+        }
+    }
+    }
