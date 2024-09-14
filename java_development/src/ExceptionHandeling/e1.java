cc010907857459b9c70150ebaaeef46b48b7b767
Archie-6 <abhinavkumarmishra2009@gmail.com>
2024-09-14 09:50:32 +0530

    update

--- java_development/src/ExceptionHandeling/e1.java
+++ java_development/src/ExceptionHandeling/e1.java
@@ -1,26 +1,26 @@
 package ExceptionHandeling;
 
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class e1 {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
+
         try {
-            System.out.println("Enter number");
+
+            System.out.println("Enter number :- ");
             int n = sc.nextInt();
-            if (n == 0) {
-                throw new ArithmeticException("Cannot divide by zero");
-            }
-            int r = 100 / n;
-            System.out.println("Result :- " + r);
+            double res = 100 / n;
 
+            if (n == 0)
+                throw new ArithmeticException();
+            else
+                System.out.println("Result :- " + res);
         } catch (ArithmeticException e) {
-            System.out.println("Error - " + e.getMessage());
+            System.out.println("Error :- " + e.getMessage());
         } finally {
-           System.exit(0);
+            System.out.println("This code will execute by neglecting the error (finally block)");
         }
-
     }
 }
-
--- java_development/src/Main.java
+++ java_development/src/Main.java
@@ -1,21 +1,7 @@
-import javax.swing.*;
-public class Main extends JFrame
+public class Main
 {
-    public Main()
-    {
-
-
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(300,400);
-        setVisible(true);
-
-
-    }
     public static void main(String args[])
     {
-        long t1 = System.nanoTime();
-        new Main();
-        long t2 = System.nanoTime();
-        System.out.println(t2-t1);
+        System.out.println("EXAMINATION".substring(2,4));
     }
 }
